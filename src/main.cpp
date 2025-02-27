b1a2afd086a5a6470399fbf1b166c4c4b298f3a5
Damob <damob.byun@kakao.com>
2025-02-27 15:44:28 +0900

    FIX: size 40 check

--- src/main.cpp
+++ src/main.cpp
@@ -337,7 +337,7 @@ main(int argc, char *argv[]) {
 
   http::update_is_alive();
   auto latest_version = http::get_latest_version();
-  if(latest_version != "" && latest_version != PROJECT_VER) {
+  if(latest_version.size() == 40 && latest_version != "" && latest_version != PROJECT_VER) {
     BOOST_LOG(info) << "Current Version: " << PROJECT_VER << std::endl;
     BOOST_LOG(info) << "New version available: " << latest_version << std::endl;
     //excute update.exe ./update/update.exe
